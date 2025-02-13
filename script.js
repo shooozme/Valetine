bdf0d9870919ed7f148232017fc22b09d1231ffd
shooozme <shooozme@gmail.com>
2025-02-13 18:43:59 -0500

    used gifs in an array in order to mess with the no button further

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 
 const noBtn = document.getElementById("noBtn");
-
+const gifEl = document.getElementById("gif");
 
 //array of messages for the noBtn
 const noButtonMessages =    ["PLEASE", "BUT WHY", "I KNWO YOU WANT TO", "I'M BEGGING", "please please please", "guh", 
                              "chex mix will suffer for this", "catastrophe", "NO NO NO NO NO NO"];
+
+//array of gifs
+const gifArray = ["assets/images/car-garn47.gif","assets/images/orange-cat-gets-flung-and-explodes-orange-cat.gif",
+                  "assets/images/spideyvivi.gif"];
 
 //function to get a random int
 function gimmeRandomInt(max) {
@@ -12,7 +16,8 @@ function gimmeRandomInt(max) {
 }
 
 //listener for button press
-noBtn.addEventListener("click", () =>
+noBtn.addEventListener("click", () => {
     noBtn.textContent = noButtonMessages[gimmeRandomInt(noButtonMessages.length)]
-
+    gifEl.src = gifArray[gimmeRandomInt(gifArray.length)]
+    }
 );
